d198fe41632820535191497da3966ce72991ce23
Gary Mac Elhinney <Gary.mac-elhinney@ucdconnect.ie>
2016-10-18 16:56:00 +0100

    Added basis for backend database

---
+++ app/src/main/java/jameshassmallarms/com/styleswap/infrastructure/DatabaseHandler.java
@@ -0,0 +1,28 @@
+package jameshassmallarms.com.styleswap.infrastructure;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by gary on 17/10/16.
+ */
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+        private static final String DATABASE_NAME = "StyleSwap";
+        private static final int DATABASE_VERSION = 7;
+
+        public DatabaseHandler(Context context) {
+                super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+        }
+}
