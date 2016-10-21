429bf69059ecd0143fd19dfa74bfa7438d84a0e2
JamesKeating <james.keating@ucdconnect.ie>
2016-10-21 23:43:37 +0100

    Uploading and downloading images from memory

--- app/src/main/java/jameshassmallarms/com/styleswap/gui/EditProfileFragment.java
+++ app/src/main/java/jameshassmallarms/com/styleswap/gui/EditProfileFragment.java
@@ -1,6 +1,10 @@
 package jameshassmallarms.com.styleswap.gui;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -9,13 +13,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 import jameshassmallarms.com.styleswap.R;
 
@@ -30,7 +44,8 @@ public class EditProfileFragment extends Fragment {
         DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
         DatabaseReference mUserRef = mRootRef.child("Users").child(userName);
         Query q = mRootRef.child("Users").equalTo("good dress");
-
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        ImageView imageView ;
 
 
         @Override
@@ -39,7 +54,7 @@ public class EditProfileFragment extends Fragment {
                 View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
                 editProfileButton = (Button) view.findViewById(R.id.editPhotoButton);
                 itemDescription = (EditText) view.findViewById(R.id.itemDescription);
-
+                imageView = (ImageView) view.findViewById(R.id.profileImage);
                 return  view;
         }
 
@@ -60,23 +75,82 @@ public class EditProfileFragment extends Fragment {
                         public void afterTextChanged(Editable s) {
                         }
                 });
+
                 editProfileButton.setOnClickListener(new View.OnClickListener() {
                         @Override
-                        public void onClick(View view) {
+                        public void onClick(View v) {
 
-                               mRootRef.child("Users").orderByChild("itemDescription").startAt("good dress").endAt("good dress").addListenerForSingleValueEvent(new ValueEventListener() {
-                                       @Override
-                                       public void onDataChange(DataSnapshot dataSnapshot) {
-
-                                               System.out.println(dataSnapshot.getValue().toString());
-                                       }
-
-                                       @Override
-                                       public void onCancelled(DatabaseError databaseError) {
-
-                                       }
-                               });
+                                //upload();
+                                download();
                         }
                 });
         }
+
+
+
+        public void upload(){
+
+                // Get the data from an ImageView as bytes
+                StorageReference picRef = storage.getReferenceFromUrl("gs://styleswap-4075c.appspot.com").child("images").child("User1");
+                imageView.setDrawingCacheEnabled(true);
+                imageView.buildDrawingCache();
+                Bitmap bitmap = imageView.getDrawingCache();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] data = baos.toByteArray();
+
+                UploadTask uploadTask = picRef.putBytes(data);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                                // Handle unsuccessful uploads
+                        }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        }
+                });
+        }
+        public void download() {
+
+                StorageReference picRef = storage.getReferenceFromUrl("gs://styleswap-4075c.appspot.com").child("images/User1");
+
+                final long ONE_MEGABYTE = 1024 * 1024;
+                picRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                        @Override
+                        public void onSuccess(byte[] bytes) {
+                                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                                System.out.println("aaa");
+                                imageView.setImageBitmap(bmp);
+
+                        }
+                }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                                // Handle any errors
+                        }
+                });
+        }
+//                //example query
+//                editProfileButton.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View view) {
+//
+//                               mRootRef.child("Users").orderByChild("itemDescription").startAt("good dress").endAt("good dress").addListenerForSingleValueEvent(new ValueEventListener() {
+//                                       @Override
+//                                       public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//                                               System.out.println(dataSnapshot.getValue().toString());
+//                                       }
+//
+//                                       @Override
+//                                       public void onCancelled(DatabaseError databaseError) {
+//
+//                                       }
+//                               });
+//                        }
+//                });
+
 }
