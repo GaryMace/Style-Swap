d0216e42ac5d3f671f25c659e385af8a6c6efdfe
Gary Mac Elhinney <Gary.mac-elhinney@ucdconnect.ie>
2016-11-19 15:18:37 +0000

    More changes to AppStartupActivity

--- app/src/main/java/jameshassmallarms/com/styleswap/base/AppStartupActivtiy.java
+++ app/src/main/java/jameshassmallarms/com/styleswap/base/AppStartupActivtiy.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -17,6 +18,8 @@ import android.widget.VideoView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import jameshassmallarms.com.styleswap.R;
 
@@ -30,6 +33,11 @@ public class AppStartupActivtiy extends Activity {
     private VideoView mIntroVid;
     private Button mLogin;
     private Button mRegister;
+
+    //Threaded Text display fields
+    private Timer mSwipeTimer;
+    private int mCurrTextBox = 0;
+    private static final int NUM_TEXT_BOXES_TO_DISPLAY = 5;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,14 +75,34 @@ public class AppStartupActivtiy extends Activity {
             }
         });
 
-       /* List<CustomObject> items = new ArrayList<>();
+       List<CustomObject> items = new ArrayList<>();
         items.add(new CustomObject("Hello", "Welcome to StyleSwap. Start swapping old dresses now."));
         items.add(new CustomObject("Discover", "Find new people to swap dresses with. Unlimited matching available."));
         items.add(new CustomObject("Chat", "Talk to new people today and start swapping dresses, your options are limitless."));
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.activity_app_startup_viewpager);
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.activity_app_startup_viewpager);
         CustomPagerAdapter customPagerAdapter = new CustomPagerAdapter(this, items);
-        viewPager.setAdapter(customPagerAdapter);*/
+        viewPager.setAdapter(customPagerAdapter);
+
+       /* //TODO: change this so that it works!!
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (mCurrTextBox == NUM_TEXT_BOXES_TO_DISPLAY) {
+                    mCurrTextBox = 0;
+                }
+                viewPager.setCurrentItem(mCurrTextBox++, true);
+            }
+        };
+
+        mSwipeTimer = new Timer();
+        mSwipeTimer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 500, 3000);*/
     }
 
     MediaPlayer.OnPreparedListener PreparedListener = new MediaPlayer.OnPreparedListener() {
